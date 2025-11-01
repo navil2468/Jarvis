3ca5064e2a12f05f958d397c66cfd2e0fcc3c83e
Navil Talukdar <naviltalukdar3@gmail.com>
2025-11-01 02:27:20 -0400

    gemini connection with api complete and chat functional

--- main.py
+++ main.py
@@ -5,4 +5,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api
+api_key = os.getenv("GEMINI_API_KEY")
+if not api_key:
+    raise ValueError("api_key environmental var not properly established!")
+
+client = genai.Client(api_key=api_key)
+print("API Key is working ")
+
+chat = client.chats.create(
+    model='gemini-2.5-flash',
+    config=types.GenerateContentConfig(
+        system_instruction="My name is Navil",
+        thinking_config=types.ThinkingConfig(
+            thinking_budget=0
+        )
+    )
+)
+
+while True:
+    txt = input("You: ")
+    if txt.lower() in ["exit", "done", "quit"]:
+        break
+    response = chat.send_message_stream(txt)
+    print("Jarvis: ", end="")
+    for wrd in response:
+        print(wrd.text, end="")
+    print()
+
