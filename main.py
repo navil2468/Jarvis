e16611770a82f3d5f19894b283a8f2ed70b47e8c
Navil Talukdar <naviltalukdar3@gmail.com>
2025-11-01 03:01:46 -0400

    audio to text implementation

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from google import genai
 from google.genai import types
 import os
 from dotenv import load_dotenv
+from RealtimeSTT import AudioToTextRecorder
 
 load_dotenv()
 
+#Loading GeminiAPIKey
 api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise ValueError("api_key environmental var not properly established!")
@@ -12,6 +14,7 @@ if not api_key:
 client = genai.Client(api_key=api_key)
 print("API Key is working ")
 
+#Create Chat with Jarvis
 chat = client.chats.create(
     model='gemini-2.5-flash',
     config=types.GenerateContentConfig(
@@ -22,9 +25,16 @@ chat = client.chats.create(
     )
 )
 
+recorder = AudioToTextRecorder(model='tiny.en', language='en', spinner='False')
+
+
+# Run whatever is said to Jarvis
 while True:
-    txt = input("You: ")
-    if txt.lower() in ["exit", "done", "quit"]:
+    #txt = input("You: ")
+    print("You: ", end="")
+    txt = recorder.text()
+    print(txt)
+    if txt.lower() in ["go to sleep", "shut down", "exit"]:
         break
     response = chat.send_message_stream(txt)
     print("Jarvis: ", end="")
@@ -32,3 +42,4 @@ while True:
         print(wrd.text, end="")
     print()
 
+recorder.shutdown()
